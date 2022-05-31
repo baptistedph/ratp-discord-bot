1bf37bb1f4d3d56080cbd141f36611e910e0d997
Baptiste Dauphouy <bdauphouy@gmail.com>
2022-05-31 21:48:18 +0200

    feat: first commit

---
+++ Client.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+from os import getenv
+from dotenv import load_dotenv
+from lib.Ratp import Ratp
+
+ratp = Ratp()
+
+load_dotenv()
+
+token = getenv('SECRET_TOKEN')
+
+bot = commands.Bot(command_prefix = '!')
+
+@bot.event
+async def on_ready():
+  print('running')
+
+@bot.command()
+async def lines(ctx, type):  
+  lines = ratp.get_lines(type)
+
+  used_codes = []
+
+  embed = discord.Embed(title = "Toutes les lignes", color = 0xffffff)
+
+  for line in lines:
+    code = line['code']
+
+    if code in used_codes:
+      used_codes.append(code)
+      continue
+    
+    used_codes.append(code)
+    embed.add_field(name = line['name'], value = line['directions'])
+
+  await ctx.reply(embed = embed)
+
+@bot.command()
+async def stations(ctx, type, code):
+  stations = ratp.get_stations(type, code)
+
+  stations_list = []
+
+  for station in stations:
+    stations_list.append(station['name'])
+
+  embed = discord.Embed(title = f"Toutes les stations de la ligne {code}", description = '\n'.join(stations_list), color = 0xffffff)
+
+  await ctx.reply(embed = embed)
+
+@bot.command()
+async def traffic(ctx, type, code):
+  traffic = ratp.get_traffic(type, code)
+
+  embed = discord.Embed(title = f"Le traffic sur la ligne {code}", description = traffic, color = 0xffffff)
+
+  await ctx.reply(embed = embed)
+
+@bot.command()
+async def schedules(ctx, type, code, station):
+  schedules = ratp.get_schedules(type, code, station)
+
+  embed = discord.Embed(title = f"Les prochains trains à la station {station} de la ligne {code}", color = 0xffffff)
+
+  for schedule in schedules:
+    embed.add_field(name = 'Heure', value = schedule['message'])
+    embed.add_field(name = 'Destination', value = schedule['destination'])
+
+  await ctx.reply(embed = embed)
+
+bot.run(token)
---
+++ lib/Ratp.py
@@ -0,0 +1,37 @@
+import requests
+
+class Ratp:
+  def __init__(self):
+    self.url = 'https://api-ratp.pierre-grimaud.fr/v4'
+
+
+  def __fetch(self, route):
+    r = requests.get(f'{self.url}{route}')
+
+    return r.json()
+
+  def get_destinations(self, code):
+    return self.__fetch(f'/destinations/{self.__get_type(code)}/{code}')
+
+  def get_lines(self, type):
+    return self.__fetch(f'/lines/{type}')['result'][type]
+
+  def get_stations(self, type, code):
+    return self.__fetch(f'/stations/{type}/{code}')['result']['stations']
+
+  def get_traffic(self, type, code):
+    return self.__fetch(f'/traffic/{type}/{code}')['result']['message']
+  
+  def get_schedules(self, type, code, station, way = 'aller-retour'):
+    if way == 'aller':
+      parsed_way = 'A'
+    elif way == 'retour':
+      parsed_way = 'R'
+    elif way == 'aller-retour':
+      parsed_way = 'A+R'
+
+    return self.__fetch(f'/schedules/{type}/{code}/{station}/{parsed_way}')['result']['schedules']
+  
+
+
+
